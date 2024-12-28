39b1980e06c57e4457025bb697175afea6377700
Alexis Inthanavong <ainthana@bess-f4r4s9.clusters.42paris.fr>
2024-12-28 16:10:27 +0100

    update but doesnt work

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ainthana <ainthana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 11:07:28 by ainthana          #+#    #+#             */
-/*   Updated: 2024/12/27 20:26:17 by ainthana         ###   ########.fr       */
+/*   Updated: 2024/12/28 16:07:46 by ainthana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,22 +43,22 @@ char	*read_static(char *line)
 	char	*stash;
 	size_t	i;
 	size_t	j;
-	size_t	len_line;
 	
 	i = 0;
 	j = 0;
 	while (line && line[i] != '\n' && line[i])
 		i++;
-	if (line[i])
+	if (line[i] == '\n' && line[i + 1])
 		i++;
 	stash = malloc(sizeof(char) * (ft_strlen(line) - i + 1));
 	if (!stash)
 		return (NULL);
 	while (line[i])
 	{
-		stash[j] = line[i + j];
-		line[i + j] = '\0';
+		stash[j] = line[i];
+		// line[i] = '\0';
 		j++;
+		i++;
 	}
 	stash[j] = '\0';
 	return (stash);		
@@ -68,28 +68,37 @@ char *get_next_line(int fd)
 {
 	static char	*stash;
 	char		*line;
-	size_t		i;
-	
-	i = 0;
+
 	line = read_line(fd, stash);
+	// printf("la ligne est : %s\n", line);
 	stash = read_static(line);
+	// printf("la stash est : %s\n", stash);
 	return (line);
 }
 
-int	main()
-{
-	char	*str = NULL;
-	int	fd;
+// int	main()
+// {
+// 	char	*str = NULL;
+// 	int			fd;
+// 	size_t		i;
 
-	fd = open("file1.txt", O_RDONLY);
-	str = get_next_line(fd);
-	printf("%s", str);
-	free(str);
-	str = get_next_line(fd);
-	printf("%s", str);
-	free(str);
-	str = get_next_line(fd);
-	printf("%s", str);
-	free(str);
-	close(fd);
-}
+// 	i = 0;
+// 	fd = open("file1.txt", O_RDONLY);
+// 	while (i < 2)
+// 	{
+// 		str = get_next_line(fd);
+// 		printf("%s", str);
+// 		free(str);
+// 		i++;
+// 	}
+// 	close(fd);
+// }
+
+// int	main(void)
+// {
+// 	char	*st;
+
+// 	st = "salut\nyone";
+// 	printf("%s\n",read_static(st));
+// 	return (0);
+// }
